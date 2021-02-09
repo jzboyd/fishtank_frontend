91626e3ad1e6fcfade5c3dac836f4ccad0fa0986
Joel Zisholtz <joelzisholtz@Joels-MBP.attlocal.net>
2021-02-09 15:19:38 -0600

    new

---
+++ navigation/tabs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
+import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs';
+
+import { Home } from "../screens";
+
+
+const Tab = createBottomTabNavigator();
+
+const Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen 
+      name="Home" 
+      component={HomeScreen}
+      options={{ tabBarIcon: ({focused}) =>
+     }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
+export default Tabs;
